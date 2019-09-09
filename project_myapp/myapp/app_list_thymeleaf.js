643f45e342b2645729edfbaf8c3ffd02d35b5713
ljh <dla1434@naver.com>
2019-09-10 00:06:46 +0900

    express-thymeleaf 적용 완료

---
+++ project_myapp/myapp/app_list_thymeleaf.js
@@ -0,0 +1,74 @@
+
+var express = require('express');
+var expressThymeleaf = require('express-thymeleaf');
+var TemplateEngine = require('thymeleaf').TemplateEngine;
+
+// import express from 'express';
+// import expressThymeleaf from 'express-thymeleaf';
+// import {TemplateEngine} from 'thymeleaf';
+
+var app = express();
+
+var fs = require('fs');
+var walk = require('walk');
+var path = require('path');
+
+app.use(express.static('public'));
+
+var templateEngine = new TemplateEngine();
+app.engine('html', expressThymeleaf(templateEngine));
+app.set('view engine', 'html');
+app.set('views', './views');
+
+app.get('/', function(req, res){
+	res.send('<h1>Hello HomePage</h1>');
+})
+
+app.get('/list/html/', function(req, res) {
+	var walker = walk.walk('public/html', {followLinks: false});
+
+	var files = [];
+	walker.on('file', function(root, stat, next){
+		rootReplacedName = root.replace('public/html\\', '').replace('\\', '/')
+		console.log('rootReplacedName : ', rootReplacedName, 'stat.name', stat.name)
+		// '/'가 아닌 w로 경로가 표시되서 보기 안 좋아서 바꿈
+		// files.push(path.join(rootReplacedName,stat.name));
+		files.push(rootReplacedName + '/' + stat.name);
+		next();
+	})
+
+	walker.on('errors', function(root, nodeStatArray, next) {
+		res.status(500).send('Internal Server Error');
+	});
+	
+	//file 처리가 다 완료되면 호출된다...
+		//res.render 밑에 정의했더니..파일이 읽는 먼저 처리되서 []로 나옴
+		//파일에 대한 목록을 표시하고 싶다면 이 메소드 하위에 res.render를 호출해야 한다.
+	walker.on('end', function() {
+		files.shift()
+		res.render('list', {topics:files});
+	});
+
+	// var test = [];
+	// test.push('111.html')
+	// test.push('222.html')
+	// console.log('files =============================',test, typeof(test))
+	// res.render('list', {topics:test});
+})
+
+//디렉토리 내에 모든 폴더를 읽어오기 위해서 walk로 변경했다.
+// app.get('/list/html/', function(req, res) {
+// 	console.log('/list/html')
+//   fs.readdir('public/html', function(err,files){
+//     if(err){
+//       console.log("first"+err);
+//       res.status(500).send('Internal Server Error');
+// 		}
+// 		console.log('files =============================',files)
+//     res.render('list', {topics:files});
+//   });
+// });
+
+app.listen(3000, function(){
+	console.log('Connected 3000 port');
+});
--- project_myapp/readme.md
+++ project_myapp/readme.md
@@ -2,18 +2,32 @@
 # 1. 생활코딩_인프런_Nodejs를 이용해 만든 웹애플리케이션 만들기
 1. nodejs
 2. express
-3. pug
-
+3. template engine
+    pug : myapp_xxxx 시작 모든 프로젝트 기본 템플릿 엔진
+    express-thymeleaf : myapp_thymeleaf에 적용
 ---
 
-## 2. 목차
-1. myapp_0001_default  
+## 2. 주요 프로젝트
+2. myapp_thymeleaf
     ```
-    > express 생성 및 http server 기동 완료
-    0001. Express install.txt
-    0002. SuperVisor install.txt
-    0003. Http server 띄우기.txt
-    0004. Static 리소스 폴더 지정하기.txt
+    1. express-thymeleaf 적용
+        1. myapp 프로젝트는 pug 기반으로 작성한 반면 이 프로젝트는 html 기반에 express-thymeleaf tempate engine을 적용하였다.
+    2. app.get('/index', function(req, res){
+        public 폴더에서 폴더목록만 보여준다.(depth1)
+    3. app.get('/list/:folder/', function(req, res) {
+        /index 페이지에서 링크를 선택하면 public의 하위 폴더에 모든 파일 목록을 보여준다.
+        ex) html 클릭 시 public/html 폴더내의 모든 파일을 보여주고..링크 클릭 시 실행도 가능
+    ```
+
+1. myapp
+    ```
+    1. /list/html 요청시 public/html 폴더의 모든 html 파일을 보여준다.
+        화면에서 클릭 시 html 실행도 가능하게 링크 작성 완료
+    ```
+
+## 3. 순차적 예제 프로젝트
+3. myapp_0003
+    ```
     ```
     
 2. myapp_0002
@@ -24,19 +38,11 @@
     0007. View_Templete_Pug_상세 사용법.txt
     ```
 
-3. myapp_0003
+1. myapp_0001_default  
     ```
+    > express 생성 및 http server 기동 완료
+    0001. Express install.txt
+    0002. SuperVisor install.txt
+    0003. Http server 띄우기.txt
+    0004. Static 리소스 폴더 지정하기.txt
     ```
-
-3. myapp_0003
-    ```
-    ```
-    
-4. myapp_0005
-    ```
-    ```
-    
-5. myapp_0005
-    ```
-    ```
-
